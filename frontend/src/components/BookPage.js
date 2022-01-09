c08c76b7d880a23717d0c395c011aa73bb09bb2f
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-01-09 13:45:02 -0800

    Fix text messages for ANY

--- frontend/src/components/BookPage.js
+++ frontend/src/components/BookPage.js
@@ -173,7 +173,7 @@ export default class BookPage extends Component {
         { this.state.not_found ? "" :
           <Grid item xs={12} align="center">
             <Typography component="h5" variant="h5">
-              You are {this.state.type == 0 ? " selling " : " buying "} BTC for {this.state.currencyCode}
+              You are {this.state.type == 0 ? " buying " : (this.state.type == 1 ? " selling ":" checking ")} BTC for {this.state.currencyCode}
             </Typography>
           </Grid>
           }
