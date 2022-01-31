18d5cac16a3358bcb40ef60b31bb037473dc31e3
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-01-31 11:12:49 -0800

    Add timeout for db writes to avoid sqlite database locked error

--- robosats/settings.py
+++ robosats/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,  # in seconds
+            }
     }
 }
 
