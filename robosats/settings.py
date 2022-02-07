14ae3d0c2ec8ac05bc9a2a9b07cc1d045455c0ae
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-02-06 16:15:45 -0800

    Prepare for collectstatic

--- robosats/settings.py
+++ robosats/settings.py
@@ -10,12 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
