0b7542bf68c11577ea3ca72184bf9831c7a46047
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-02-20 04:03:53 -0800

    Add postgresql envvars

--- robosats/settings.py
+++ robosats/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = "robosats.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "example",
-        'HOST': '127.0.0.1',
-        "PORT": "5432",
+        "NAME": config("POSTGRES_NAME"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        'HOST': config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
