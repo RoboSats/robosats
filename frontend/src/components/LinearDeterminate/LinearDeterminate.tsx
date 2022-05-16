523d89948212b41c5a48eaed07474ed54055b0c7
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-05-16 01:34:03 -0700

    Add import React on LinearDeterminate

--- frontend/src/components/LinearDeterminate/LinearDeterminate.tsx
+++ frontend/src/components/LinearDeterminate/LinearDeterminate.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, LinearProgress } from "@mui/material"
 import { calcTimeDelta } from 'react-countdown';
 
