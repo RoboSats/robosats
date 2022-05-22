59d8d325b2bad9ce1f965c440e804e1f987fe233
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-05-22 10:46:04 -0700

    Add utility functions for keypair generation, encrypt and decrypt messages

---
+++ frontend/src/utils/pgp.js
@@ -0,0 +1,66 @@
+import * as openpgp from 'openpgp/lightweight';
+
+// Generate KeyPair. Private Key is encrypted with the highEntropyToken
+export async function genKeys(highEntropyToken) {
+
+  const keyPair = await openpgp.generateKey({
+    type: 'ecc', // Type of the key, defaults to ECC
+    curve: 'curve25519', // ECC curve name, defaults to curve25519
+    userIDs: [{name: 'RoboSats Avatar'}],
+    passphrase: highEntropyToken,
+    format: 'armored'
+  })
+  
+  console.log(keyPair)
+  
+  const publicKeyArmored = keyPair.publicKey;
+  const privateKeyArmored = keyPair.privateKey; // encrypted private key
+
+  return {publicKeyArmored: publicKeyArmored, privateKeyArmored: privateKeyArmored}
+};
+
+// Encrypt and sign a message
+export async function encryptMessage(plainMessage, publicKeyArmored, privateKeyArmored, passphrase) {
+
+  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+  const privateKey = await openpgp.decryptKey({
+      privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
+      passphrase
+  });
+
+  const encryptedMessage = await openpgp.encrypt({
+      message: await openpgp.createMessage({ text: plainMessage }), // input as Message object, message must be string
+      encryptionKeys: publicKey,
+      signingKeys: privateKey // optional
+  });
+
+  return encryptedMessage; // '-----BEGIN PGP MESSAGE ... END PGP MESSAGE-----'
+};
+
+// Decrypt and check signature of a message
+export async function decryptMessage(encryptedMessage, publicKeyArmored, privateKeyArmored, passphrase) {
+
+  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+  const privateKey = await openpgp.decryptKey({
+      privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
+      passphrase
+  });
+
+  const message = await openpgp.readMessage({
+      armoredMessage: encryptedMessage // parse armored message
+  });
+  const { data: decrypted, signatures } = await openpgp.decrypt({
+      message,
+      verificationKeys: publicKey, // optional
+      decryptionKeys: privateKey
+  });
+  
+  // check signature validity (signed messages only)
+  try {
+    await signatures[0].verified; // throws on invalid signature
+    console.log('Signature is valid');
+    return {decryptedMessage: decrypted, validSignature: true}
+  } catch (e) {
+    return {decryptedMessage: decrypted, validSignature: false};
+  }
+};
