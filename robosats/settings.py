588691c4c8870a36c1814e6774dadf64acbaba79
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2022-08-09 02:44:29 -0700

    Add new payment methods and rename posgres_db envvar

--- robosats/settings.py
+++ robosats/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = "robosats.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_NAME"),
+        "NAME": config("POSTGRES_DB"),
         "USER": config("POSTGRES_USER"),
         "PASSWORD": config("POSTGRES_PASSWORD"),
         'HOST': config("POSTGRES_HOST"),
