35088d4748b9735e4920089eadab69e26c78af83
KoalaSat <yv1vtrul@duck.com>
2022-09-12 08:51:16 +0200

    WIP

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import {WebView, WebViewMessageEvent} from 'react-native-webview';
 import {SafeAreaView, Text, Platform} from 'react-native';
-// import Tor from "react-native-tor";
+import Tor from 'react-native-tor';
 
 // Initialize the module
-// const tor = Tor();
+const tor = Tor();
 
-// const makeTorRequest = async()=>{
-//     // Start the daemon and socks proxy (no need for Orbot and yes iOS supported!)
-//     await tor.startIfNotStarted();
+const makeTorRequest = async () => {
+  // Start the daemon and socks proxy (no need for Orbot and yes iOS supported!)
+  await tor.startIfNotStarted();
 
-//     try{
-//        // Use built in client to make REST calls to .onion urls routed through the Sock5 proxy !
-//        const resp = await tor.get('http://robosats6tkf3eva7x2voqso3a5wcorsnw34jveyxfqi2fu7oyheasid.onion/api/info');
-//        return resp
-//     } catch(error){
-//       // Catch a network or server error like you normally with any other fetch library
-//     }
-// }
+  try {
+    // Use built in client to make REST calls to .onion urls routed through the Sock5 proxy !
+    const resp = await tor.get(
+      'http://robosats6tkf3eva7x2voqso3a5wcorsnw34jveyxfqi2fu7oyheasid.onion/api/info',
+    );
+
+    return resp;
+  } catch (error) {
+    // Catch a network or server error like you normally with any other fetch library
+  }
+};
+makeTorRequest();
 
 const App = () => {
   // var uri =
