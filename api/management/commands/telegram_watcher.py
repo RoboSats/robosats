d8f917b19c57197f1c985ce70b6e4690db1b73af
Kote <45825337+Gravity2106@users.noreply.github.com>
2023-02-09 01:37:44 +0100

    Update telegram_watcher.py

--- api/management/commands/telegram_watcher.py
+++ api/management/commands/telegram_watcher.py
@@ -2,6 +2,7 @@ import time
 
 from decouple import config
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from api.messages import Telegram
 from api.models import Profile
 from api.utils import get_session
@@ -19,54 +20,55 @@ class Command(BaseCommand):
     telegram = Telegram()
 
     def handle(self, *args, **options):
-        """Infinite loop to check for telegram updates.
-        If it finds a new user (/start), enables it's taker found
-        notification and sends a 'Hey {username} {order_id}' message back"""
-
         offset = 0
         while True:
             time.sleep(self.rest)
+            params = {"offset": offset + 1, "timeout": 5}
             try:
+                response = self.session.get(self.updates_url, params=params)
+                if response.status_code != 200:
+                    with open('error.log', 'a') as f:
+                        f.write(f"Error getting updates, status code: {response.status_code}\n")
+                    continue
+                response = response.json()
                 response = self.session.get(self.updates_url, params=params).json()
             except Exception as e:
                 with open('error.log', 'a') as f:
                     f.write(f"Error getting updates: {e}\n{traceback.format_exc()}\n")
                 continue
 
-            params = {"offset": offset + 1, "timeout": 5}
-            response = self.session.get(self.updates_url, params=params).json()
             if not response["result"]:
                 continue
             for result in response["result"]:
-                if not result.get("message")("text"):
+                if not result.get("message") or not result.get("message").get("text"):
                     continue
-                if not result["message"].get("text") or not result["message"]["text"].startswith("/start"):
-                    if result["message"]["text"] == "/start":
-                        token = input("Please enter your token: ")
-                        profile = Profile.objects.filter(telegram_token=token).first()
-                        if not profile:
-                            print(f"No profile with token {token}")
-                            continue
-                if len(result["message"]["text"].split(" ")) < 2:
-                    print("Invalid format. It should be: /start <token>")
-                    print("Remember that you can find your token after activate Telegram notifications. It is in the Tor address after start=<OnlythisistheToken>")
+                message = result["message"]["text"]
+                if not message or not message.startswith("/start"):
                     continue
-                    token = result["message"]["text"].split(" ")[-1]
-                    profile = Profile.objects.filter(telegram_token=token).first()
-                    if not profile:
-                        print(f"No profile with token {token}")
-                        continue
+                parts = message.split(" ")
+                if len(parts) < 2:
+                    self.telegram.send_message(chat_id=result["message"]["from"]["id"],
+                                                text="Invalid format. It should be: /start <token>")
+                    self.telegram.send_message(chat_id=result["message"]["from"]["id"],
+                                                text="Remember that you can find your token after activating Telegram notifications. It is in the Tor address after start=<OnlythisistheToken>")
+                    continue
+                token = parts[-1]
+                profile = Profile.objects.filter(telegram_token=token).first()
+                if not profile:
+                    self.telegram.send_message(chat_id=result["message"]["from"]["id"],
+                                                text=f"No profile with token {token}")
+                    continue
 
-                    attempts = 5
-                    while attempts >= 0:
-                        try:
+                attempts = 5
+                while attempts >= 0:
+                    try:
+                        with transaction.atomic():
                             profile.telegram_chat_id = result["message"]["from"]["id"]
                             profile.telegram_lang_code = result["message"]["from"]["language_code"]
                             self.telegram.welcome(profile.user)
                             profile.telegram_enabled = True
                             profile.save()
-                            break
-                        except Exception:
-                            time.sleep(5)
-                            attempts -= 1
+                    except Exception:
+                        time.sleep(5)
+                        attempts -= 1
             offset = response["result"][-1]["update_id"]
