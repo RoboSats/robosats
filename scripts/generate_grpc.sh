7a5342136eecdf1114045eeb2e72bc9bab743cf6
daywalker90 <admin@noserver4u.de>
2023-04-11 17:19:18 +0200

    init

--- scripts/generate_grpc.sh
+++ scripts/generate_grpc.sh
@@ -24,6 +24,12 @@ python3 -m grpc_tools.protoc --proto_path=googleapis:. --python_out=. --grpc_pyt
 curl -o verrpc.proto -s https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/verrpc/verrpc.proto
 python3 -m grpc_tools.protoc --proto_path=googleapis:. --python_out=. --grpc_python_out=. verrpc.proto
 
+# generate cln grpc definitions
+curl -o node.proto -s https://raw.githubusercontent.com/daywalker90/lightning/master/cln-grpc/proto/node.proto
+python3 -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. node.proto
+curl -o primitives.proto -s https://raw.githubusercontent.com/daywalker90/lightning/master/cln-grpc/proto/primitives.proto
+python3 -m grpc_tools.protoc --proto_path=. --python_out=. --grpc_python_out=. primitives.proto
+
 # delete googleapis
 rm -r googleapis
 
