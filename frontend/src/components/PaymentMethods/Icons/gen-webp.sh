13e79df7c956afb3c908561c8e37f2ba3e591091
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2023-05-10 14:51:14 -0700

    Decrease payment icons size, increase sharpness

--- frontend/src/components/PaymentMethods/Icons/gen-webp.sh
+++ frontend/src/components/PaymentMethods/Icons/gen-webp.sh
@@ -7,6 +7,6 @@ do
   echo ""
   title=$(echo $f | sed 's/\.[^.]*$//')
   echo "Processing $f..."
-  cwebp "$f" -q 30 -resize 225 225 -o webp/"$(echo $f | rev | cut -d"." -f2- | rev).webp" < /dev/null
+  cwebp "$f" -q 80 -psnr 40 -resize 80 80 -o webp/"$(echo $f | rev | cut -d"." -f2- | rev).webp" < /dev/null
   echo ""
 done
