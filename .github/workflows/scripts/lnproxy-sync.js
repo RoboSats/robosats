12e5ccd2632a7cbd27b2dccecd4343930e97d5d6
+shyfire131 <shyfire131@shyfire131.net>
2023-05-15 11:29:45 -0600

    Update sync script to be append-only

--- .github/workflows/scripts/lnproxy-sync.js
+++ .github/workflows/scripts/lnproxy-sync.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 
-let rawRelays = JSON.parse(fs.readFileSync('./lnproxy_relays.json'));
-let formattedRelays = [];
+let incomingRelays = JSON.parse(fs.readFileSync('./lnproxy_tmplist.json'));
+let existingRelays = JSON.parse(fs.readFileSync('./frontend/static/lnproxies.json'))
+
+let newRelays = [];
 
 let torCount = 0;
 let i2pCount = 0;
 let clearnetCount = 0;
 
-for (let url of rawRelays) {
+//Merge relay lists. URL is the unique ID used to merge records and only inserts supported. No updates or deletes
+let existingRelayURLs = existingRelays.map((relay) => relay.url);
+let newIncomingRelays = incomingRelays.filter((relay)=> existingRelayURLs.indexOf(relay) === -1)
+
+for (let url of newIncomingRelays) {
   let relayType;
   const LNPROXY_API_PATH = '/spec'
   const fqdn = url.replace(LNPROXY_API_PATH, '');
@@ -26,7 +32,7 @@ for (let url of rawRelays) {
 
   let relayName = `${relayType}${relayType === "TOR" ? torCount : ''}${relayType === "I2P" ? i2pCount : ''}${relayType === "Clearnet" ? clearnetCount : ''} ${url.split('/')[2].substring(0,6)}`
 
-  formattedRelays.push({
+  newRelays.push({
     name: relayName,
     url: url,
     relayType: relayType,
@@ -34,4 +40,7 @@ for (let url of rawRelays) {
   });
 }
 
-fs.writeFileSync('./frontend/static/lnproxies.json', JSON.stringify(formattedRelays, null, 2));
+if (newRelays.length > 0) {
+  existingRelays.push(...newRelays);
+  fs.writeFileSync('./frontend/static/lnproxies.json', JSON.stringify(existingRelays, null, 2));
+}
