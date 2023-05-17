883f8688360b031f9889ed0a652624fb6a507041
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2023-05-17 02:27:19 -0700

    Minor fixes on dev setup start up

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# Copy node_modules if it doesn't exist
+# Print first start up message when node_modules does not exist
 if [ ! -d "/usr/src/frontend/node_modules" ]; then
     echo "Looks like the first run of this container. Node modules were not detected on the attached volume, copying them into the attached volume."
-    cp -R /tmp/node_modules /usr/src/frontend/node_modules
 fi
+
+# copy modules without replacing
+cp -R -n /tmp/node_modules /usr/src/frontend/node_modules
 
 exec "$@"
