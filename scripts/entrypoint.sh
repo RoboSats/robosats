2bb0b4d7bf5944c195f5a50ee28b48361dddbeb4
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2023-05-17 02:27:19 -0700

    Minor fixes on dev setup start up

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,11 +10,14 @@ else
     python manage.py collectstatic --noinput
 fi
 
-# Copy pb2/grpc files if they do exist
+# Print first start up message when pb2/grpc files if they do exist
 if [ ! -f "/usr/src/robosats/api/lightning/lightning_pb2.py" ]; then
-    echo "Looks like the first run of this container. pb2 and gRPC files were not detected on the attached volume, copying them into the attached volume /robosats/api/node ."
-    cp -R /tmp/* /usr/src/robosats/api/lightning/
+    echo "Looks like the first run of this container. pb2 and gRPC files were not detected on the attached volume, copying them into the attached volume /robosats/api/lightning ."
 fi
+
+# Copy and overwrite all existing pb2/grpc files always. Therefore, running
+# /scripts/generate_grpc.sh only makes sense if done during Dockerfile build
+cp -R /tmp/* /usr/src/robosats/api/lightning/
 
 # Start server / gunicorn / daphne / command
 exec "$@"
