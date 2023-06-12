8fed02c5f4feb3e808c8ee90cf6716a769e8cb80
Daven <53495928+dapsavoie@users.noreply.github.com>
2023-06-12 11:06:39 +0100

    add bd to worldflags

--- frontend/src/components/Icons/WorldFlags.tsx
+++ frontend/src/components/Icons/WorldFlags.tsx
@@ -88,6 +88,7 @@ const FlagWithProps = ({ code, width = '1.428em', height = '1.428em' }: Props): 
   if (code === 'UGX') flag = <Flags.UG {...defaultProps} />;
   if (code === 'RSD') flag = <Flags.RS {...defaultProps} />;
   if (code === 'IRT') flag = <Flags.IR {...defaultProps} />;
+  if (code === 'BDT') flag = <Flags.BD {...defaultProps} />;
   if (code === 'ANY') flag = <EarthIcon {...defaultProps} />;
   if (code === 'XAU') flag = <GoldIcon {...defaultProps} />;
   if (code === 'BTC') flag = <SwapCallsIcon color='primary' />;
