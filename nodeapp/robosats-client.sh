9d87bf6bd6a00e804c0cb8831f6b7be2ad8c900e
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2023-11-03 11:42:25 +0000

    Add coordinator: Satstralia

--- nodeapp/robosats-client.sh
+++ nodeapp/robosats-client.sh
@@ -9,7 +9,7 @@
 # Every robosat coordinators needs a tor socat bridge.
 
 ###############################
-# Experimental Coordinator
+# Experimental
 # Mainnet
 mainnet_exp_onion=robosats6tkf3eva7x2voqso3a5wcorsnw34jveyxfqi2fu7oyheasid.onion
 mainnet_exp_port=101
@@ -21,7 +21,7 @@ mainnet_exp_socat="socat tcp4-LISTEN:${mainnet_exp_port},reuseaddr,fork,keepaliv
 testnet_exp_socat="socat tcp4-LISTEN:${testnet_exp_port},reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:${TOR_PROXY_IP:-127.0.0.1}:${testnet_exp_onion}:80,socksport=${TOR_PROXY_PORT:-9050}"
 
 ################################
-# Temple of Sats Coordinator
+# Temple of Sats
 # Mainnet
 mainnet_temple_onion=zixoneehmz7z2ctsnpuubcni4kxw5gp6fkyzjd2spo5atie5awiuwmyd.onion
 mainnet_temple_port=102
@@ -33,5 +33,18 @@ mainnet_temple_socat="socat tcp4-LISTEN:${mainnet_temple_port},reuseaddr,fork,ke
 testnet_temple_socat="socat tcp4-LISTEN:${testnet_temple_port},reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:${TOR_PROXY_IP:-127.0.0.1}:${testnet_temple_onion}:80,socksport=${TOR_PROXY_PORT:-9050}"
 
 
+################################
+# Satstralia
+# Mainnet
+mainnet_satstralia_onion=satstraoq35jffvkgpfoqld32nzw2siuvowanruindbfojowpwsjdgad.onion
+mainnet_satstralia_port=103
+# Testnet
+testnet_satstralia_onion=qu6xztmzhlve6nxbb77jldek53pvhkaltz6seni7wq6g6yyj233qp4yd.onion
+testnet_satstralia_port=1003
+# socat cmd
+mainnet_satstralia_socat="socat tcp4-LISTEN:${mainnet_satstralia_port},reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:${TOR_PROXY_IP:-127.0.0.1}:${mainnet_satstralia_onion}:80,socksport=${TOR_PROXY_PORT:-9050}"
+testnet_satstralia_socat="socat tcp4-LISTEN:${testnet_satstralia_port},reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:${TOR_PROXY_IP:-127.0.0.1}:${testnet_satstralia_onion}:80,socksport=${TOR_PROXY_PORT:-9050}"
+
+
 # RUN!
-$mainnet_exp_socat & $testnet_exp_socat & $mainnet_temple_socat & $testnet_temple_socat & nginx
+$mainnet_exp_socat & $testnet_exp_socat & $mainnet_temple_socat & $testnet_temple_socat & $mainnet_satstralia_socat & $testnet_satstralia_socat& nginx
