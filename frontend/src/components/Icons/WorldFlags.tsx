07830fd2fd8fb9e60a6ae6bd060fbb7230c3d98c
Reckless_Satoshi <reckless.satoshi@protonmail.com>
2023-11-30 16:13:38 +0000

    Add currency Albanian LEK

--- frontend/src/components/Icons/WorldFlags.tsx
+++ frontend/src/components/Icons/WorldFlags.tsx
@@ -89,6 +89,7 @@ const FlagWithProps = ({ code, width = '1.428em', height = '1.428em' }: Props): 
   if (code === 'RSD') flag = <Flags.RS {...defaultProps} />;
   if (code === 'IRT') flag = <Flags.IR {...defaultProps} />;
   if (code === 'BDT') flag = <Flags.BD {...defaultProps} />;
+  if (code === 'ALL') flag = <Flags.AL {...defaultProps} />;
   if (code === 'ANY') flag = <EarthIcon {...defaultProps} />;
   if (code === 'XAU') flag = <GoldIcon {...defaultProps} />;
   if (code === 'BTC') flag = <SwapCallsIcon color='primary' />;
