b70ab717da0626bdd4d69915213d1e881bce5f48
aftermath2 <cxbvnlrzfjwi@protonmail.com>
2024-01-20 11:48:32 -0300

    Format sats amount in millionths

--- frontend/src/components/BookTable/index.tsx
+++ frontend/src/components/BookTable/index.tsx
@@ -321,11 +321,11 @@ const BookTable = ({
       type: 'number',
       width: width * fontSize,
       renderCell: (params: any) => {
-        const amount = fav.mode === 'swap' ? params.row.amount * 100000 : params.row.amount;
+        const amount = fav.mode === 'swap' ? params.row.amount * 100 : params.row.amount;
         const minAmount =
-          fav.mode === 'swap' ? params.row.min_amount * 100000 : params.row.min_amount;
+          fav.mode === 'swap' ? params.row.min_amount * 100 : params.row.min_amount;
         const maxAmount =
-          fav.mode === 'swap' ? params.row.max_amount * 100000 : params.row.max_amount;
+          fav.mode === 'swap' ? params.row.max_amount * 100 : params.row.max_amount;
         return (
           <div
             style={{ cursor: 'pointer' }}
@@ -334,7 +334,7 @@ const BookTable = ({
             }}
           >
             {amountToString(amount, params.row.has_range, minAmount, maxAmount) +
-              (fav.mode === 'swap' ? 'K Sats' : '')}
+              (fav.mode === 'swap' ? 'M Sats' : '')}
           </div>
         );
       },
