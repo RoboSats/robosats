58d21d93e9bec4a5127bd6035ee566db82ecd1fc
koalasat <koalasat@satstralia.com>
2024-08-09 17:06:41 +0200

    Better sync urls

--- docker/strfry/sync.sh
+++ docker/strfry/sync.sh
@@ -3,5 +3,5 @@
 filters='{"kinds":[38383]}'
 
 while IFS= read -r line; do
-  /app/strfry --config /etc/strfry.conf sync ws://${line}/nostr --filter "$filters" --dir both
+  /app/strfry --config /etc/strfry.conf sync ${line} --filter "$filters" --dir both
 done < /app/onion_urls.txt
