7804009d2d759cf7a9aff8fa17c57e2f23b34316
koalasat <koalasat@satstralia.com>
2024-09-17 17:28:48 +0200

    fix tests

--- tests/test_frontend_fetch.py
+++ tests/test_frontend_fetch.py
@@ -10,10 +10,10 @@ class FrontendFetchTest(TestCase):
         path = reverse("basic")
         response = self.client.get(path)
         self.assertContains(response, "<html>")
-        self.assertContains(response, "-basic")
+        self.assertContains(response, "RoboSats -")
 
     def test_pro_frontend_url_content(self):
         path = reverse("pro")
         response = self.client.get(path)
         self.assertContains(response, "<html>")
-        self.assertContains(response, "-pro")
+        self.assertContains(response, "RoboSats PRO -")
