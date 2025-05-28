791854474ca4cb6b361aa61c6cd451272dcf6037
koalasat <koalasat@satstralia.com>
2025-05-28 13:15:55 +0200

    Fix chat older versions

--- frontend/src/components/TradeBox/EncryptedChat/index.tsx
+++ frontend/src/components/TradeBox/EncryptedChat/index.tsx
@@ -43,7 +43,7 @@ const EncryptedChat: React.FC<Props> = ({
   status,
 }: Props): React.JSX.Element => {
   const [turtleMode, setTurtleMode] = useState<boolean>(false);
-  const { hostUrl, settings } = useContext<UseAppStoreType>(AppContext);
+  const { settings } = useContext<UseAppStoreType>(AppContext);
   const { garage } = useContext<UseGarageStoreType>(GarageContext);
   const { federation } = useContext<UseFederationStoreType>(FederationContext);
 
@@ -72,12 +72,13 @@ const EncryptedChat: React.FC<Props> = ({
   const sendToNostr = (content: string): void => {
     const slot = garage.getSlot();
     const coordinator = federation.getCoordinator(order.shortAlias);
+    const publicKey = order.is_maker ? order.taker_nostr_pubkey : order.maker_nostr_pubkey;
 
-    if (!slot?.nostrSecKey) return;
+    if (!slot?.nostrSecKey || !publicKey) return;
 
     const recipient = {
-      publicKey: order.is_maker ? order.taker_nostr_pubkey : order.maker_nostr_pubkey,
-      relayUrl: coordinator.getRelayUrl(settings.network, hostUrl, settings.selfhostedClient),
+      publicKey,
+      relayUrl: coordinator.getRelayUrl(settings.network),
     };
 
     const wrappedEvent = nip17.wrapEvent(slot?.nostrSecKey, recipient, content);
