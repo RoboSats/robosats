cc4ea2cd72d2d4c0840fad1bdf60dfc083168385
Eudald Gubert i Roldan <hola@eudald.gr>
2026-02-18 12:11:37 +0100

    refactor: remove unused slotUpdatedAt dependency and optimize onSlotUpdate calls in Slot model

--- frontend/src/basic/OrderPage/index.tsx
+++ frontend/src/basic/OrderPage/index.tsx
@@ -25,8 +25,13 @@ import { type UseGarageStoreType, GarageContext } from '../../contexts/GarageCon
 import { genBase62Token } from '../../utils';
 
 const OrderPage = (): React.JSX.Element => {
-  const { windowSize, setOpen, acknowledgedWarning, setAcknowledgedWarning, navbarHeight } =
-    useContext<UseAppStoreType>(AppContext);
+  const {
+    windowSize,
+    setOpen,
+    acknowledgedWarning,
+    setAcknowledgedWarning,
+    navbarHeight,
+  } = useContext<UseAppStoreType>(AppContext);
   const { federation } = useContext<UseFederationStoreType>(FederationContext);
   const { garage } = useContext<UseGarageStoreType>(GarageContext);
   const { t } = useTranslation();
