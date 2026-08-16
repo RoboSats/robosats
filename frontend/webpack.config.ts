bb04b28cb7f955ce6f7e0dd52fb60d09955a07e0
KoalaSat <git@koalasat.xyz>
2026-08-16 10:56:11 +0200

    Downgrade babel

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -25,11 +25,7 @@ const config: Configuration = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env',
-              ['@babel/preset-react', { runtime: 'automatic' }],
-              ['@babel/preset-typescript', { onlyRemoveTypeImports: false }],
-            ],
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
           },
         },
       },
