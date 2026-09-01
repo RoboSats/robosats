fc293c2b49d5def7c15b3e8703047e51169e6b94
Alice <alice7b@proton.me>
2026-09-01 15:58:00 +0000

    Replace format_html with mark_safe

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from statistics import median
 from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, User
-from django.utils.html import format_html
+from django.utils.html import mark_safe
 from django_admin_relation_links import AdminChangeLinksMixin
 from rest_framework.authtoken.admin import TokenAdmin
 from rest_framework.authtoken.models import TokenProxy
@@ -130,7 +130,7 @@ class OrderAdmin(AdminChangeLinksMixin, admin.ModelAdmin):
     readonly_fields = ("reference", "_logs")
 
     def _logs(self, obj):
-        return format_html(render_order_logs(obj.logs))
+        return mark_safe(render_order_logs(obj.logs))
 
     actions = [
         "cancel_public_order",
